172aadeca259de7df724dfb2579c178f1ce9b1d4
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-10-04 22:00:19 +0530

    check

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -3,3 +3,11 @@ for(const code of coding){
     console.log(`I love ${code}`)
 }
 // --- IGNORE ---
+for(let i=0; i<5; i++){
+    console.log(i);
+}
+
+// Related portions of 04_control_flow/two.js:
+if(isUserloggedIn){
+    console.log("You are logged in");
+}   
