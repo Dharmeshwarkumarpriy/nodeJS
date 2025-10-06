12b9e5d2e62290c32e607c6b2571c0e1bfd88be9
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-10-06 08:37:49 +0530

    splice add

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -26,14 +26,14 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // slice, splice
 
-console.log("A ", myArr);
+console.log("A ", myArr); //[0,1,2,3,4,5]
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+console.log(myn1); // [1,2]
+console.log("B ", myArr); //[0,1,2,3,4,5] // slice value print myArr ...
 
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+console.log("C ", myArr); // print after remove splice value [0,4,5] ...
+console.log(myn2); // print splice value [1,2,3] ...
