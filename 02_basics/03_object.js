fc93da446d7358bd646a317eb4d849a2fd15ea16
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-10-11 22:18:01 +0530

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,1 +1,40 @@
 // singaltan ...
+
+// object leterals
+
+const mySym=Symbol('mySym')
+
+const jsUser ={
+    name:"dharmy",
+    "full name":"dharmendra kumar",
+    [mySym]: "myvalue",
+    age:"18",
+    location:"india",
+    email:"dk@gmail.com",
+    isLoggedIn:true,
+    LastLogin: ["monday","saturday"]
+}
+
+console.log(jsUser.email); // dot notation..
+// console.log(jsUser[email]) // undefined
+console.log(jsUser["full name"]) // object key is not valid identifier..
+console.log(jsUser[mySym]); // myvalue // symbol key access.. 
+
+jsUser.email="dhamry@gmail.com" // update object.. 
+
+//   Object.freeze(jsUser) // freeze object..
+
+jsUser.email="priy@gmail.com" // not update....
+console.log(jsUser);
+
+jsUser.greetings=function(){
+    console.log("hello js user"); // method..
+}
+
+jsUser.greetingTwo=function(){
+    console.log(`hello, ${this.name}`);
+} // method..
+
+console.log(jsUser.greetings()); // hello js user // return undefined..
+
+console.log(jsUser.greetingTwo()); // undefined // return nothing..
