e1aa37ce2f626a02a5ef4bb6584b1e7894b2affe
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-10-31 15:10:22 +0530

    fivenote

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,5 +1,5 @@
-const coding =["js","python","c++","java","ruby"]
-for(const code of coding){
-    console.log(`I love ${code}`)
+const coding =["js","python","c++","java","ruby"] // array of strings..
+for(const code of coding){ // for...of loop..
+    console.log(`I love ${code}`) // I love js // I love python // I love c++ // I love java // I love ruby //
 }
 // --- IGNORE ---
