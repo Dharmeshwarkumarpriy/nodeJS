f1d12895538581d6fa2d8e36c483538955685855
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-11-05 09:06:48 +0530

    two change

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,20 +1,20 @@
 // while
 
-let index = 0
-while(index <= 10){
-    console.log(`value of index is: ${index}`);
-    index= index + 2
+let index = 0 // initialization
+while(index <= 10){ // condition
+    console.log(`value of index is: ${index}`); // code block / statement
+    index= index + 2 // updation
 }
 
 let myArray=['flash','batman',"superman"]
 let arr=0
-while(arr<myArray.length){
-    console.log('value is: ',myArray[arr]);
-    arr =arr+1
+while(arr<myArray.length){ // condition
+    console.log('value is: ',myArray[arr]); // code block / statement
+    arr =arr+1 // updation
 }
 
-let scose=11
-do{
-    console.log('score is: ',scose);
-    scose++
-}while(scose<=10);
+let scose=11 // initialization
+do{ // condition    
+    console.log('score is: ',scose); // code block / statement
+    scose++ // updation
+}while(scose<=10); // condition check // at least one time the code block will be executed
