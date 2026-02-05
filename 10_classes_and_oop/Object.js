5f80c314adfc9ba40a1480597cc19b20984cd516
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2026-02-05 19:53:10 +0530

    update

--- 10_classes_and_oop/Object.js
+++ 10_classes_and_oop/Object.js
@@ -1,27 +1,26 @@
-function multipleBy5(num) {
-    return num*5
+function multipleBy5(num) { // function object
+    return num*5 // return value
 }
 
-multipleBy5.power=2
+multipleBy5.power=2 // add property to function object
 
-console.log(multipleBy5(5));
-console.log(multipleBy5.power);
-console.log(multipleBy5.prototype);
+console.log(multipleBy5(5)); // call function
+console.log(multipleBy5.power); // access property
+console.log(multipleBy5.prototype); // access prototype property
 
-function createUser(username,score) {
-    this.username=username
-    this.score=score
+function createUser(username,score) { // constructor function
+    this.username=username // this keyword
+    this.score=score // this keyword
 }
 
-createUser.prototype.increment=function() {
-    this.score++
+createUser.prototype.increment=function() { // method
+    this.score++ // access property
 }
 
-createUser.prototype.printMe=function () {
-    console.log(`score is ${this.score}`);
+createUser.prototype.printMe=function () { // method
+    console.log(`score is ${this.score}`); // access property
     
 }
-const chai=new createUser("chai",23)
-const tea = createUser("tea",234)
-
-chai.printMe()
+const chai=new createUser("chai",23) // create object
+const tea = createUser("tea",234) // create object without new keyword
+chai.printMe() // call method
--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,13 +1,13 @@
-const User={
+const User={ /// object literal
     userName: "dharmy",
     LoginCount: 8,
     signedIn: true,
 
-    getUserDetails: function(){
+    getUserDetails: function(){ // method
         console.log("get user details from database");
 
         console.log("print username...");
-        console.log(`username: ${this.userName}`);
+        console.log(`username: ${this.userName}`); // this key word use to access object property
         
         console.log("print all constant...");
         console.log(this);
@@ -17,18 +17,18 @@ const User={
 }
 
 console.log(User.userName);
-console.log(User.getUserDetails())
+console.log(User.getUserDetails()) // call method
 
-console.log("globle print all constant...");
-console.log(this);
+console.log("globle print all constant..."); // this key word in globle scope
+console.log(this); // window object
 
 
-function user(username, Logincount, isloggedin) {
+function user(username, Logincount, isloggedin) { // function declaration 
     this.Logincount=Logincount;
-    this.username=username;
+    this.username=username; // this key word
     this.isloggedin=isloggedin;
 
-    this.greeting=function () {
+    this.greeting=function () { // method
         console.log(`welcome ${this.username}`);
         
     }
@@ -36,17 +36,17 @@ function user(username, Logincount, isloggedin) {
     return this;
 }
 
-const userOne=user("priy",2,true)
+const userOne=user("priy",2,true) //
 const userTwo=user("dharmy..",43,false)
 // create object, call constructor key word, this key word inject orgument, call arg by in fun....
 const userOne1=new user("priy",2,true)
 const userTwo1=new user("dharmy..",43,false)
 
-console.log(userOne);
+console.log(userOne); // print object
 console.log(userTwo);
 
 console.log(userOne1);
 console.log(userTwo1);
 
-console.log(userOne.constructor);
+console.log(userOne.constructor); // print function
 console.log(userOne1.constructor);
